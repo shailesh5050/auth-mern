b61d28c85b44c222d318d05e17eb07e5347a37bb
shailesh5050 <shaileshkuswaha1@gmail.com>
2024-02-09 15:29:25 +0530

    Added : Server is Created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./About";
-import Home from "./Home";
-import SignIn from "./SignIn";
-import SignUp from "./SignUp";
-import Profile from "./Profile";
-
+import About from "./pages/About";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SIgnUp";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
 export const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -18,3 +19,4 @@ export const App = () => {
     </BrowserRouter>
   );
 };
+export default App;
