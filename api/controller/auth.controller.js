5f694fd00a50e88e2650e0df3a677351f68f1a6a
shailesh5050 <shaileshkuswaha1@gmail.com>
2024-02-10 13:14:41 +0530

    Added : SignIN Api routes

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcrypt";
+import jwt from "jsonwebtoken";
 export async function signup(req, res, next) {
   const { username, email, password } = req.body;
   console.log(req.body);
@@ -18,6 +19,35 @@ export async function signup(req, res, next) {
       .json({ ...userRes._doc, message: "User Created Successfully" })
       .status(201);
   } catch (err) {
+    next(errorHandler(500, err.message));
+  }
+}
+
+export async function signin(req, res, next) {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      next(errorHandler(401, "User not found"));
+      return;
+    }
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
+    if (!isPasswordValid) {
+      next(errorHandler(401, "Invalid Credentials"));
+      return;
+    }
+    const token = jwt.sign(
+      { email: user.email, id: user._id },
+      process.env.JWT
+    );
+    // Exclude password from user data
+    const userData = { ...user._doc };
+    delete userData.password;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json({ ...userData, message: "Sign In Success" });
+  } catch (error) {
     next(errorHandler(500, err.message));
   }
 }
