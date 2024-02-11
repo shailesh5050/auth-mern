20600b9444a58c29191f88c2e0d9d463428107d3
shailesh5050 <shaileshkuswaha1@gmail.com>
2024-02-11 12:03:10 +0530

    Added : Image Upload

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,110 @@
-const Profile = () => {
-  return <div>Profile</div>;
-};
+import { useSelector } from "react-redux";
+import { useRef, useState, useEffect } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  uploadBytesResumable,
+  ref,
+} from "firebase/storage";
+import { authFirebase } from "../firebase";
+export default function Profile() {
+  const fileRef = useRef(null);
+  const [image, setImage] = useState(undefined);
+  const [imageError, setImageError] = useState(false);
+  const [formData, setFormData] = useState({});
+  const { currentUser, loading, error } = useSelector((state) => state.user);
+  const [imagePercetage, setImagePercetage] = useState(0);
+  useEffect(() => {
+    if (image) {
+      handleImageUpload(image);
+    }
+  }, [image]);
+  async function handleImageUpload(image) {
+    const storage = getStorage(authFirebase);
+    const fileName = new Date().getTime() + "-" + image.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
 
-export default Profile;
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        setImagePercetage(Math.round(progress));
+      },
+      (error) => {
+        console.log(error);
+      },
+      // download url
+      async () => {
+        const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+        setFormData((prev) => ({ ...prev, profilePicture: downloadURL }));
+      }
+    );
+
+    console.log(image);
+  }
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+          onChange={(e) => setImage(e.target.files[0])}
+        />
+        <img
+          src={formData.profilePicture || currentUser.profilePicture}
+          alt="profile"
+          className="h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2"
+          onClick={() => fileRef.current.click()}
+        />
+        <p className="text-sm self-center">
+          {imageError ? (
+            <span className="text-red-700">
+              Error uploading image (file size must be less than 2 MB)
+            </span>
+          ) : (
+            ""
+          )}
+        </p>
+        <p className="text-sm self-center text-green-300">
+          {imagePercetage > 0 && "Image Uploaded "}
+          {imagePercetage > 0 && imagePercetage + " % "}
+        </p>
+        <input
+          defaultValue={currentUser.username}
+          type="text"
+          id="username"
+          placeholder="Username"
+          className="bg-slate-100 rounded-lg p-3"
+        />
+        <input
+          defaultValue={currentUser.email}
+          type="email"
+          id="email"
+          placeholder="Email"
+          className="bg-slate-100 rounded-lg p-3"
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          className="bg-slate-100 rounded-lg p-3"
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          {loading ? "Loading..." : "Update"}
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer">Sign out</span>
+      </div>
+      <p className="text-red-700 mt-5">{error && "Something went wrong!"}</p>
+      <p className="text-green-700 mt-5">User is updated successfully!</p>
+    </div>
+  );
+}
